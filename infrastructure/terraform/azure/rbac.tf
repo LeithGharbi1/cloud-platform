2a3479af2c8fc67401c50fa08d4a7812ded228da
LeithGharbi1 <leith.gharbi@esprit.tn>
2026-09-05 13:26:49 +0100

    granting user role

--- infrastructure/terraform/azure/rbac.tf
+++ infrastructure/terraform/azure/rbac.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "aks_cluster_admin" {
   role_definition_name = "Azure Kubernetes Service RBAC Cluster Admin"
   principal_id         = data.azurerm_client_config.current.object_id
 }
+
+resource "azurerm_role_assignment" "pipeline_aks_cluster_user" {
+  scope                = azurerm_kubernetes_cluster.cloud_platform.id
+  role_definition_name = "Azure Kubernetes Service Cluster User Role"
+  principal_id         = data.azurerm_user_assigned_identity.pipeline.principal_id
+}
